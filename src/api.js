337a1afa3ca3888c93a7b3508754b5d0b9dd85f2
DmytriyKireev <35657933+Dmitriy-8-Kireev@users.noreply.github.com>
2019-01-19 21:29:23 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export const search = query =>
-  fetch(`http://api.tvmaze.com/search/shows?q=${query}`, {
+  fetch(`https://api.tvmaze.com/search/shows?q=${query}`, {
     method: 'GET',
     mode: 'cors',
   })
@@ -7,7 +7,7 @@ export const search = query =>
     .then(shows => shows.map(show => show.show));
 
 export const show = showId =>
-  fetch(`http://api.tvmaze.com/shows/${showId}?embed=cast`, {
+  fetch(`https://api.tvmaze.com/shows/${showId}?embed=cast`, {
     method: 'GET',
     mode: 'cors',
   }).then(response => response.json());
